56e10bbf18d30902414d52ec756ea89e72c177d5
互传 <ls676514702@163.com>
2017-04-21 12:57:26 +0800

    first commit

---
+++ 趋势图Demo/HCChartFormatter.swift
@@ -0,0 +1,13 @@
+//
+//  HCChartFormatter.swift
+//  趋势图Demo
+//
+//  Created by 互传 on 2017/4/21.
+//  Copyright © 2017年 互传. All rights reserved.
+//
+
+import UIKit
+
+class HCChartFormatter: NSObject {
+
+}
